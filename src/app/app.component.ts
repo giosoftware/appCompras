c736118083beef8db79133a6f5934109354ff188
Sergio Martín <giosoftware@gmail.com>
2019-01-03 11:21:21 +0100

    AngularFireStorage was added.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AngularFireStorage } from 'angularfire2/storage';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  constructor(private storage: AngularFireStorage) { }
+  ngOnInit() {
+  }
 }
